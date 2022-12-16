208cf02b27c19fe2201f34d02be7225c4b5d8e9e
Ldre3 <116470430+Ldre3@users.noreply.github.com>
2022-12-16 13:29:54 +0100

    v4. Para sacar el método seleccionamos el códgio que queremos sacar, click derecho refactor, introduce, method, ahi le damos nombre y lo seleccionamos como público.

--- src/es/calcularPI/CalcPI.java
+++ src/es/calcularPI/CalcPI.java
@@ -8,6 +8,10 @@ public class CalcPI {
      public static void main(String[] args) {
          final int NUM_DIG = 10;  // Para sacar la constante seleccionamos el número a extraer, click derecho refactor, introduce, varibale y ahí le ponemos el nombre, publica y que reemplace las otras ocurrencias       
         MathContext mc = new MathContext(NUM_DIG);         
+        compute(NUM_DIG, mc);
+    }
+    //Para sacar el método seleccionamos el códgio que queremos sacar, click derecho refactor, introduce, method, ahi le damos nombre y lo seleccionamos como público.
+    public static void compute(final int NUM_DIG, MathContext mc) {
         //meter en el método compute
         BigDecimal pi = new BigDecimal(0);
         BigDecimal limit = new BigDecimal(1).movePointLeft(NUM_DIG);
