7aa641fcabc2e43d4d112a2c7871533e0032973b
Ldre3 <116470430+Ldre3@users.noreply.github.com>
2022-12-16 13:37:59 +0100

    v5. Para mover el método a una clase, creamos la clase, seleccionamos el metodo click derecho, mover, y seleccionamos a qué clase lo queremos mover

---
+++ src/es/calcularPI/ClasePadre.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.calcularPI;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+public class ClasePadre {
+    //Para mover el método a una clase, creamos la clase, seleccionamos el metodo click derecho, mover, y seleccionamos a qué clase lo queremos mover
+    //Para sacar el método seleccionamos el códgio que queremos sacar, click derecho refactor, introduce, method, ahi le damos nombre y lo seleccionamos como público.
+    public static BigDecimal piFunction(int k, MathContext mc) {
+        //meter en el método piFunction
+        int k8 = 8 * k;
+        BigDecimal val1 = new BigDecimal(4);
+        val1 = val1.divide(new BigDecimal(k8 + 1), mc);
+        BigDecimal val2 = new BigDecimal(-2);
+        val2 = val2.divide(new BigDecimal(k8 + 4), mc);
+        BigDecimal val3 = new BigDecimal(-1);
+        val3 = val3.divide(new BigDecimal(k8 + 5), mc);
+        BigDecimal val4 = new BigDecimal(-1);
+        val4 = val4.divide(new BigDecimal(k8 + 6), mc);
+        BigDecimal val = val1;
+        val = val.add(val2);
+        val = val.add(val3);
+        val = val.add(val4);
+        BigDecimal multiplier = new BigDecimal(16);
+        multiplier = multiplier.pow(k);
+        BigDecimal one = new BigDecimal(1);
+        multiplier = one.divide(multiplier, mc);
+        val = val.multiply(multiplier);
+        BigDecimal piK = val;
+        return piK;
+    }
+    
+}
